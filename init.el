8a0b0cf34154e21b729f46d26caa232f777ce972
rguthrie3 <rguthrie6595@gmail.com>
2019-10-30 23:08:08 -0500

    Move a lot of stuff into use-package forms

--- init.el
+++ init.el
@@ -48,18 +48,40 @@
 ;; EVIL
 ;; =======================================================================
 (defun split-horizontally-and-move-to-window ()
+  "Split a window and also move to it"
   (interactive)
   (split-window-vertically)
   (windmove-down))
 
 (defun split-vertically-and-move-to-window ()
+  "Split a window and also move to it"
   (interactive)
   (split-window-horizontally)
   (windmove-right))
 
 (use-package evil
   :ensure t
-  :config (evil-mode))
+  :config (evil-mode)
+  (evil-define-key '(normal motion) 'global "\C-l" 'windmove-right)
+  (evil-define-key '(normal motion) 'global "\C-k" 'windmove-up)
+  (evil-define-key '(normal motion) 'global "\C-j" 'windmove-down)
+  (evil-define-key '(normal motion) 'global "\C-h" 'windmove-left)
+  (evil-define-key '(normal motion) 'global "\C-d" 'evil-scroll-down)
+  (evil-define-key '(normal motion) 'global "\C-u" 'evil-scroll-up)
+  (evil-define-key '(normal motion insert) 'global (kbd "C-x w") 'delete-other-windows)
+  (evil-define-key '(normal motion) 'global (kbd "0") 'evil-first-non-blank)
+  (evil-define-key 'insert 'global "\C-w" 'evil-delete-backward-word)
+
+  (evil-define-key '(normal motion) 'global (kbd ",") nil)
+  (evil-define-key '(normal motion) 'global (kbd ",q") 'evil-tab-sensitive-quit)
+  (evil-define-key '(normal motion) 'global (kbd ",t") 'dired-in-new-tab)
+  (evil-define-key '(normal motion) 'global (kbd ",r") 'evil-tabs-current-buffer-to-tab)
+  (evil-define-key 'normal 'global (kbd ",w") 'save-buffer)
+  (evil-define-key 'normal 'global (kbd ",s") 'split-horizontally-and-move-to-window)
+  (evil-define-key 'normal 'global (kbd ",v") 'split-vertically-and-move-to-window)
+  (evil-define-key 'normal 'global (kbd ",c") 'comment-or-uncomment-region)
+  (evil-define-key 'normal 'global (kbd ",m") 'compile))
+
 
 (use-package evil-tabs
   :ensure t
@@ -80,26 +102,6 @@
   (let ((root (or (projectile-project-root) default-directory)))
     (evil-ex-call-command nil "tabnew" nil)
     (dired root)))
-
-(evil-define-key '(normal motion) 'global "\C-l" 'windmove-right)
-(evil-define-key '(normal motion) 'global "\C-k" 'windmove-up)
-(evil-define-key '(normal motion) 'global "\C-j" 'windmove-down)
-(evil-define-key '(normal motion) 'global "\C-h" 'windmove-left)
-(evil-define-key '(normal motion) 'global "\C-d" 'evil-scroll-down)
-(evil-define-key '(normal motion) 'global "\C-u" 'evil-scroll-up)
-(evil-define-key '(normal motion insert) 'global (kbd "C-x w") 'delete-other-windows)
-(evil-define-key '(normal motion) 'global (kbd "0") 'evil-first-non-blank)
-(evil-define-key 'insert 'global "\C-w" 'evil-delete-backward-word)
-
-(evil-define-key '(normal motion) 'global (kbd ",") nil)
-(evil-define-key '(normal motion) 'global (kbd ",q") 'evil-tab-sensitive-quit)
-(evil-define-key '(normal motion) 'global (kbd ",t") 'dired-in-new-tab)
-(evil-define-key '(normal motion) 'global (kbd ",r") 'evil-tabs-current-buffer-to-tab)
-(evil-define-key 'normal 'global (kbd ",w") 'save-buffer)
-(evil-define-key 'normal 'global (kbd ",s") 'split-horizontally-and-move-to-window)
-(evil-define-key 'normal 'global (kbd ",v") 'split-vertically-and-move-to-window)
-(evil-define-key 'normal 'global (kbd ",c") 'comment-or-uncomment-region)
-(evil-define-key 'normal 'global (kbd ",m") 'compile)
 
 ;; =======================================================================
 ;; CLEAN UP STANDARD EMACS CONF
@@ -122,11 +124,17 @@
 ;; Rebind describe key somewhere else
 (define-key global-map (kbd "C-x k") 'describe-key)
 
+;; Better switch to buffer binding
+(evil-define-key '(normal motion) 'global (kbd "C-b") 'switch-to-buffer)
+
 ;; =======================================================================
 ;; STANDARD EMACS CONFIGS
 ;; =======================================================================
 ;; Load colorscheme
 (load-theme 'darcula t)
+
+;; Disable annoying bell
+(setq ring-bell-function 'ignore)
 
 ;; Highlight matching paren when cursor is over it
 (show-paren-mode t)
@@ -175,7 +183,8 @@
 (require 'dired)
 
 ;; Dired sort directories together at the top
-(setq dired-listing-switches "-la --group-directories-first")
+;; TODO: This doesn't work on OSX
+;(setq dired-listing-switches "-la --group-directories-first")
 
 ;; Always refresh Dired from the fs state
 (setq dired-auto-revert-buffer t)
@@ -223,6 +232,7 @@
 ;; =======================================================================
 ;; CLOJURE
 ;; =======================================================================
+;; Useful paredit hydra that puts everything under one prefix
 (defhydra hydra-paredit (global-map "C-e")
   "paredit"
   ("l" paredit-forward-slurp-sexp "slurp-forward")
@@ -239,10 +249,14 @@
   ("j" paredit-join-sexps "join")
   ("k" kill-sexp "kill"))
 
+;; Makes normal mode operations preserve paren balance
+(use-package evil-paredit :ensure t)
+
+;; Paredit in emacs lisp
 (add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode) (evil-paredit-mode)))
 
 (defun my-clojure-stuff ()
-  (rainbow-delimiters-mode t)
+  (rainbow-delimiters-mode t) ; Highlight matching parens
   (cider-mode t)
   (paredit-mode t)
   (clj-refactor-mode t)
@@ -254,58 +268,66 @@
   (evil-define-key 'insert 'local (kbd "}") 'paredit-close-curly)
   (evil-define-key 'insert 'local (kbd "]") 'paredit-close-square))
 
-(use-package clojure-mode
-  :ensure t)
+(use-package clojure-mode :ensure t)
 
 (use-package cider
-  :ensure t
-  :config
-  (setq cider-repl-display-in-current-window t))
+             :ensure t
+             :config
+             (setq cider-repl-display-in-current-window t)
+             ;; Cider control
+             (define-key cider-mode-map (kbd "C-c q") 'cider-quit)
+             (define-key cider-mode-map (kbd "C-c j") 'cider-jack-in)
+
+             ;; Definition lookup
+             (define-key cider-mode-map (kbd "C-c g d") 'cider-find-var)
+
+             ;; Expression evaluation
+             (define-key cider-mode-map (kbd "C-c C-e") nil)
+             (define-key cider-mode-map (kbd "C-c C-e l") 'cider-eval-last-sexp)
+             (define-key cider-mode-map (kbd "C-c C-e r") 'cider-eval-last-sexp-to-repl)
+             (define-key cider-mode-map (kbd "C-c C-e p") 'cider-insert-last-sexp-in-repl)
+             (define-key cider-mode-map (kbd "C-c C-e t") 'cider-eval-defun-at-point)
+             (define-key cider-mode-map (kbd "C-c C-e a") 'cider-eval-sexp-at-point)
+             (define-key cider-mode-map (kbd "C-c C-e d") 'cider-debug-defun-at-point)
+
+             ;; Formatting
+             (define-key cider-mode-map (kbd "C-c f r") 'cider-format-region)
+             (define-key cider-mode-map (kbd "C-c f f") 'cider-format-defun)
+
+             ;; Documentation
+             (define-key cider-mode-map (kbd "C-c C-d c") 'clojure-view-cheatsheet)
+
+             ;; Namespacing
+             (define-key cider-mode-map (kbd "C-c C-n") 'cider-repl-set-ns)
+
+             ;; Fix up REPL usage in Normal mode
+             (define-key cider-repl-mode-map (kbd "<up>") 'cider-repl-backward-input)
+             (define-key cider-repl-mode-map (kbd "<down>") 'cider-repl-forward-input)
+             ;; We define RET -> cider-repl-return in cider-repl-mode-hooks for evil reasons
+
+             ;; Renaming refactorings
+             (define-key clj-refactor-map (kbd "C-c r r f") 'cljr-rename-file-or-dir)
+             (define-key clj-refactor-map (kbd "C-c r r s") 'cljr-rename-symbol)
+             ;; Let refactorings
+             (define-key clojure-mode-map (kbd "C-c r l i") 'clojure-introduce-let)
+             (define-key clojure-mode-map (kbd "C-c r l m") 'clojure-move-to-let)
+             (define-key clj-refactor-map (kbd "C-c r l r") 'cljr-remove-let)
+             ;; Inline symbol
+             (define-key clj-refactor-map (kbd "C-c r i") 'cljr-inline-symbol))
 
 (use-package clj-refactor
   :ensure t)
+
+(defun my-cider-repl-mode-stuff ()
+  (evil-define-key '(normal motion) 'local (kbd "<up>") nil)
+  (evil-define-key '(normal motion) 'local (kbd "<down>") nil)
+  (evil-define-key '(normal motion) 'local (kbd "RET") 'cider-repl-return))
 
 (add-hook 'clojure-mode-hook 'my-clojure-stuff)
 (add-hook 'cider-mode-hook #'company-mode)
 (add-hook 'cider-repl-mode-hook #'company-mode)
 (add-hook 'cider-repl-mode-hook #'paredit-mode)
-
-;; Cider control
-(define-key cider-mode-map (kbd "C-c q") 'cider-quit)
-(define-key cider-mode-map (kbd "C-c j") 'cider-jack-in)
-
-;; Definition lookup
-(define-key cider-mode-map (kbd "C-c g d") 'cider-find-var)
-
-;; Expression evaluation
-(define-key cider-mode-map (kbd "C-c C-e") nil)
-(define-key cider-mode-map (kbd "C-c C-e l") 'cider-eval-last-sexp)
-(define-key cider-mode-map (kbd "C-c C-e r") 'cider-eval-last-sexp-to-repl)
-(define-key cider-mode-map (kbd "C-c C-e p") 'cider-insert-last-sexp-in-repl)
-(define-key cider-mode-map (kbd "C-c C-e t") 'cider-eval-defun-at-point)
-(define-key cider-mode-map (kbd "C-c C-e a") 'cider-eval-sexp-at-point)
-(define-key cider-mode-map (kbd "C-c C-e d") 'cider-debug-defun-at-point)
-
-;; Formatting
-(define-key cider-mode-map (kbd "C-c f r") 'cider-format-region)
-(define-key cider-mode-map (kbd "C-c f f") 'cider-format-defun)
-
-;; Documentation
-(define-key cider-mode-map (kbd "C-c C-d c") 'clojure-view-cheatsheet)
-
-;; Namespacing
-(define-key cider-mode-map (kbd "C-c C-n") 'cider-repl-set-ns)
-
-;; Renaming refactorings
-(define-key clj-refactor-map (kbd "C-c r r f") 'cljr-rename-file-or-dir)
-(define-key clj-refactor-map (kbd "C-c r r s") 'cljr-rename-symbol)
-;; Let refactorings
-(define-key clojure-mode-map (kbd "C-c r l i") 'clojure-introduce-let)
-(define-key clojure-mode-map (kbd "C-c r l m") 'clojure-move-to-let)
-(define-key clj-refactor-map (kbd "C-c r l r") 'cljr-remove-let)
-;; Inline symbol
-(define-key clj-refactor-map (kbd "C-c r i") 'cljr-inline-symbol)
-
+(add-hook 'cider-repl-mode-hook 'my-cider-repl-mode-stuff)
 
 ;; =======================================================================
 ;; CLANG FORMAT
@@ -329,15 +351,14 @@
 ;; =======================================================================
 (use-package helm
   :ensure t
-  :config (helm-mode))
-(evil-define-key 'normal 'global (kbd "C-x C-f") 'helm-find-files)
-(evil-define-key 'normal 'global (kbd "C-f") 'helm-find-files)
-(evil-define-key '(normal motion) 'global (kbd "C-x b") 'helm-apropos)
-(evil-define-key '(insert normal motion) 'global "\M-x" 'helm-M-x)
+  :config (helm-mode)
+  (evil-define-key 'normal 'global (kbd "C-x C-f") 'helm-find-files)
+  (evil-define-key 'normal 'global (kbd "C-f") 'helm-find-files)
+  (evil-define-key '(normal motion) 'global (kbd "C-x b") 'helm-apropos)
+  (evil-define-key '(insert normal motion) 'global "\M-x" 'helm-M-x)
+  ;; C-w to kill word when doing a helm search
+  (define-key helm-map (kbd "C-w") 'backward-kill-word))
 
-(evil-define-key '(normal motion) 'global (kbd "C-b") 'switch-to-buffer)
-
-(define-key helm-map (kbd "C-w") 'backward-kill-word)
 
 (use-package helm-swoop
   :ensure t
@@ -416,8 +437,9 @@
   :config (which-key-mode t))
 
 (use-package evil-easymotion
-  :ensure t)
-(evilem-default-keybindings "SPC")
+  :ensure t
+  :config
+  (evilem-default-keybindings "SPC"))
 
 (use-package yasnippet
   :ensure t
@@ -430,8 +452,9 @@
 
 (use-package evil-magit
   :ensure t
-  :config (evil-magit-init))
-(global-set-key (kbd "C-x g") 'magit-status)
+  :config
+  (evil-magit-init)
+  (global-set-key (kbd "C-x g") 'magit-status))
 
 ;; =======================================================================
 ;; PROJECTILE
@@ -440,42 +463,21 @@
   :ensure t
   :config
   (projectile-mode t)
-  (define-key projectile-mode-map (kbd "C-p") 'projectile-command-map))
+
+  ;; Set projectile prefix
+  (evil-define-key 'normal 'global (kbd "C-p") nil)
+  (define-key projectile-mode-map (kbd "C-p") 'projectile-command-map)
+
+  ;; Open dired at root by just typing D
+  (evil-define-key 'normal 'global (kbd "D") 'projectile-dired))
+
 (use-package helm-projectile
   :ensure t
   :config
   (helm-projectile-on)
   (setq projectile-completion-system 'helm))
 
-;; Open dired at root by just typing D
-(evil-define-key 'normal 'global (kbd "D") 'projectile-dired)
-
-(evil-define-key 'normal 'global (kbd "C-p") nil)
-
-;; =======================================================================
-;; PROJECT EXPLORER
-;; =======================================================================
-(use-package project-explorer
-  :ensure t
-  :config
-  (evil-define-key 'normal 'global (kbd ",nn") 'project-explorer-open)
-  (evil-define-key 'normal 'global (kbd ",nf") 'project-explorer-open)
-
-  (setq pe/filenotify-enabled 1)
-  (setq pe/width 32))
-
-(add-hook 'project-explorer-mode-hook (lambda ()
-  (evil-define-key '(normal motion) 'local (kbd "u") 'pe/up-element)
-  (evil-define-key '(normal motion) 'local (kbd "TAB") 'pe/tab)
-  (evil-define-key '(normal motion) 'local (kbd "a") 'pe/goto-top)
-  (evil-define-key '(normal motion) 'local (kbd "+") 'pe/create-file)
-  (evil-define-key '(normal motion) 'local (kbd "-") 'pe/delete-file)
-  (evil-define-key '(normal motion) 'local (kbd "r") 'pe/rename-file)
-  (evil-define-key '(normal motion) 'local (kbd "RET") 'pe/return)
-  (evil-define-key '(normal motion) 'local (kbd "q") 'pe/quit)
-))
-
-;; =======================================================================
+; =======================================================================
 ;; CUSTOM COMMANDS
 ;; =======================================================================
 (defun reload-emacs ()
